103ad7fd36cedcdf3572d495408ed47dce68f29e
Anthony Nahas <anthony.na@hotmail.de>
2020-07-13 20:00:23 +0200

    fix(project): changed the default project to `ngx-age-validator-demo`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-age-validator';
+  title = 'ngx-age-validator-demo';
 }
