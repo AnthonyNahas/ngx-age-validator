bde3cf35179d29406d90c01876740d663307a5b6
Anthony Nahas <anthony.na@hotmail.de>
2020-07-14 21:36:54 +0200

    fix(project): minor

--- projects/ngx-age-validator/src/lib/validator.ts
+++ projects/ngx-age-validator/src/lib/validator.ts
@@ -4,10 +4,10 @@ import * as moment from 'moment';
 export function NgxAgeValidator(min?: number, max?: number): ValidatorFn {
   return (control: AbstractControl): { [key: string]: boolean } | null => {
     const birthday: Date = control.value as Date;
-    console.log("birthday", birthday);
+    // console.log("birthday", birthday);
     if (birthday !== undefined) {
       const years = moment().diff(birthday, 'years');
-      console.log("years", years);
+      // console.log("years", years);
       if (min && min > years) {
         return {'tooJung': true}
       }
